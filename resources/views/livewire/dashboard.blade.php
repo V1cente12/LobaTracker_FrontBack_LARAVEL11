f0050904189efb7704713328068b5ba191015fd7
V1cente12 <jvicentecv@gmail.com>
2024-08-31 23:13:16 -0400

    dashboard finished

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,5 +1,6 @@
-<div>
-    <div class="p-4 bg-gradient-to-r from-green-400 to-green-500 shadow sm:rounded-lg mb-6">
+<div class="p-8 bg-gradient-to-b from-gray-100 to-gray-300 min-h-screen">
+    <div class="p-4 bg-gradient-to-r from-[#FF0000] to-[#FF3333] shadow sm:rounded-lg mb-6">
+        
         <div class="flex items-center justify-between mb-4">
             <h2 class="text-2xl font-bold text-white">Lobby de Juegos</h2>
             <button wire:click="showCreateGameModal" class="bg-yellow-400 text-gray-800 px-4 py-2 rounded-lg font-semibold hover:bg-yellow-500">
@@ -16,14 +17,22 @@
     @else
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             @foreach($games as $game)
-                <div class="bg-green-200 shadow-lg rounded-lg p-6">
-                    <h3 class="text-xl font-semibold text-gray-900 mb-2">{{ $game->name }}</h3>
-                    <p class="text-gray-700 mb-4">Creado el: {{ $game->created_at->format('d-m-Y H:i') }}</p>
-                    <p class="text-gray-700">Parada: <span class="font-semibold">{{ $game->initial_price }} Bs</span></p>
-                    <p class="text-gray-700">Reenganche: <span class="font-semibold">{{ $game->rejoin_price }} Bs</span></p>
-                    <p class="text-gray-700">Creador: <span class="font-semibold">{{ $game->creator->name }}</span></p>
+                <div class="bg-gradient-to-b from-[#199949] to-green-500 border-gray-100 shadow-lg rounded-lg p-6 transform hover:scale-105 transition-transform duration-300">
+                    <h3 class="text-xl font-semibold text-white mb-2">{{ $game->name }}</h3>
+                    <p class="text-gray-100 mb-4">
+                        <span class="font-semibold">Creado el: </span> 
+                        <span class="text-lg text-gray-200">
+                            {{ $game->created_at->format('d F Y') }}
+                        </span>
+                        <span class="text-sm text-gray-300 ml-2">
+                            a las {{ $game->created_at->format('H:i') }}
+                        </span>
+                    </p>
+                    <p class="text-gray-100">Parada: <span class="font-semibold">{{ $game->initial_price }} Bs</span></p>
+                    <p class="text-gray-100">Reenganche: <span class="font-semibold">{{ $game->rejoin_price }} Bs</span></p>
+                    <p class="text-gray-100">Creador: <span class="font-semibold">{{ $game->creator->name }}</span></p>
                     <div class="mt-4 flex justify-end">
-                        <button wire:click="showJoinGameModal({{ $game->id }})" class="bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600">
+                        <button wire:click="showJoinGameModal({{ $game->id }})" class="bg-yellow-400 text-gray-800 px-4 py-2 rounded-lg font-semibold hover:bg-yellow-500">
                             Unirse
                         </button>
                     </div>
@@ -33,52 +42,70 @@
     @endif
 
     <!-- Modal para Crear Juego -->
-    <div x-data="{ open: @entangle('showToCreateGameModal') }" x-show="open" @keydown.escape.window="open = false" class="fixed inset-0 z-50 flex items-center justify-center bg-gray-900 bg-opacity-75">
-        <div class="bg-white dark:bg-gray-800 p-8 rounded-lg shadow-xl w-full max-w-lg">
-            <h2 class="text-xl font-semibold text-gray-900 dark:text-gray-200 mb-6">Crear Nuevo Juego</h2>
+    <div x-data="{ open: @entangle('showToCreateGameModal') }"
+    x-show="open"
+    @keydown.escape.window="open = false"
+    x-transition:enter="transition ease-out duration-300"
+    x-transition:enter-start="opacity-0 scale-90"
+    x-transition:enter-end="opacity-100 scale-100"
+    x-transition:leave="transition ease-in duration-200"
+    x-transition:leave-start="opacity-100 scale-100"
+    x-transition:leave-end="opacity-0 scale-95"
+    class="fixed inset-0 z-50 flex items-center justify-center bg-gray-900 bg-opacity-75">
+        <div class="bg-gradient-to-b from-[#199949] to-green-500 p-8 rounded-lg shadow-xl w-full max-w-lg">
+        <h2 class="text-xl font-semibold text-white mb-6">Crear Nuevo Juego</h2>
             <form wire:submit.prevent="createGame" class="space-y-6">
                 <div>
-                    <label for="gameName" class="block text-sm font-medium text-gray-700 dark:text-gray-300">Nombre del Juego</label>
-                    <input type="text" id="gameName" wire:model="gameName" class="mt-1 block w-full p-2 bg-gray-100 dark:bg-gray-700 text-gray-900 dark:text-gray-200 border border-gray-300 dark:border-gray-600 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500" required>
+                    <label for="gameName" class="block text-base font-medium text-white">Nombre del Juego</label>
+                    <input type="text" id="gameName" wire:model="gameName" class="mt-1 block w-full p-2 bg-gray-100 text-gray-900 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500" required>
                     @error('gameName') <span class="text-red-500 text-sm">{{ $message }}</span> @enderror
                 </div>
 
                 <div>
-                    <label for="initialPrice" class="block text-sm font-medium text-gray-700 dark:text-gray-300">Precio de Parada</label>
-                    <input type="number" id="initialPrice" wire:model="initialPrice" class="mt-1 block w-full p-2 bg-gray-100 dark:bg-gray-700 text-gray-900 dark:text-gray-200 border border-gray-300 dark:border-gray-600 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500" step="0.01" required>
+                    <label for="initialPrice" class="block text-base font-medium text-white">Precio de Parada</label>
+                    <input type="number" id="initialPrice" wire:model="initialPrice" class="mt-1 block w-full p-2 bg-gray-100 text-gray-900 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500" step="0.01" required>
                     @error('initialPrice') <span class="text-red-500 text-sm">{{ $message }}</span> @enderror
                 </div>
 
                 <div>
-                    <label for="rejoinPrice" class="block text-sm font-medium text-gray-700 dark:text-gray-300">Precio de Reenganche</label>
-                    <input type="number" id="rejoinPrice" wire:model="rejoinPrice" class="mt-1 block w-full p-2 bg-gray-100 dark:bg-gray-700 text-gray-900 dark:text-gray-200 border border-gray-300 dark:border-gray-600 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500" step="0.01" required>
+                    <label for="rejoinPrice" class="block text-base font-medium text-white">Precio de Reenganche</label>
+                    <input type="number" id="rejoinPrice" wire:model="rejoinPrice" class="mt-1 block w-full p-2 bg-gray-100 text-gray-900 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500" step="0.01" required>
                     @error('rejoinPrice') <span class="text-red-500 text-sm">{{ $message }}</span> @enderror
                 </div>
 
                 <div class="flex justify-end space-x-4">
                     <button type="button" wire:click="resetModal" class="px-4 py-2 bg-gray-500 text-white rounded-lg hover:bg-gray-600 focus:ring focus:ring-gray-300">Cancelar</button>
-                    <button type="submit" class="px-4 py-2 bg-indigo-600 text-white rounded-lg hover:bg-indigo-700 focus:ring focus:ring-indigo-300">Crear Juego</button>
+                    <button type="submit" class="px-4 py-2 bg-yellow-400 text-gray-800 rounded-lg hover:bg-yellow-500 focus:ring focus:ring-yellow-300">Crear Juego</button>
                 </div>
             </form>
         </div>
     </div>
-
+    
     <!-- Modal para Unirse a un Juego -->
-    <div x-data="{ open: @entangle('showToJoinGameModal') }" x-show="open" @keydown.escape.window="open = false" class="fixed inset-0 z-50 flex items-center justify-center bg-gray-900 bg-opacity-75">
-        <div class="bg-white dark:bg-gray-800 p-8 rounded-lg shadow-xl w-full max-w-lg">
-            <h2 class="text-xl font-semibold text-gray-900 dark:text-gray-200 mb-6">Unirse al Juego</h2>
+    <div x-data="{ open: @entangle('showToJoinGameModal') }"
+    x-show="open"
+    @keydown.escape.window="open = false"
+    x-transition:enter="transition ease-out duration-300"
+    x-transition:enter-start="opacity-0 scale-90"
+    x-transition:enter-end="opacity-100 scale-100"
+    x-transition:leave="transition ease-in duration-200"
+    x-transition:leave-start="opacity-100 scale-100"
+    x-transition:leave-end="opacity-0 scale-95"
+    class="fixed inset-0 z-50 flex items-center justify-center bg-gray-900 bg-opacity-75">
+        <div class="bg-gradient-to-b from-[#199949] to-green-500 p-8 rounded-lg shadow-xl w-full max-w-lg">
+        <h2 class="text-xl font-semibold text-white mb-6">Unirse al Juego</h2>
             <form wire:submit.prevent="joinGame" class="space-y-6">
                 <input type="hidden" wire:model="selectedGameId">
 
                 <div>
-                    <label for="nickname" class="block text-sm font-medium text-gray-700 dark:text-gray-300">Nickname</label>
-                    <input type="text" id="nickname" wire:model="nickname" class="mt-1 block w-full p-2 bg-gray-100 dark:bg-gray-700 text-gray-900 dark:text-gray-200 border border-gray-300 dark:border-gray-600 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500" required>
+                    <label for="nickname" class="block text-base font-medium text-white">Nickname</label>
+                    <input type="text" id="nickname" wire:model="nickname" class="mt-1 block w-full p-2 bg-gray-100 text-gray-900 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500" required>
                     @error('nickname') <span class="text-red-500 text-sm">{{ $message }}</span> @enderror
                 </div>
 
                 <div class="flex justify-end space-x-4">
                     <button type="button" wire:click="resetModal" class="px-4 py-2 bg-gray-500 text-white rounded-lg hover:bg-gray-600 focus:ring focus:ring-gray-300">Cancelar</button>
-                    <button type="submit" class="px-4 py-2 bg-indigo-600 text-white rounded-lg hover:bg-indigo-700 focus:ring focus:ring-indigo-300">Unirse</button>
+                    <button type="submit" class="px-4 py-2 bg-yellow-400 text-gray-800 rounded-lg hover:bg-yellow-500 focus:ring focus:ring-yellow-300">Unirse</button>
                 </div>
             </form>
         </div>
--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -1,4 +1,4 @@
-<nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
+<nav x-data="{ open: false }" class="bg-gradient-to-b from-[#013d1c] to-green-500 border-b border-gray-100">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
@@ -25,9 +25,8 @@
                         <x-dropdown align="right" width="60">
                             <x-slot name="trigger">
                                 <span class="inline-flex rounded-md">
-                                    <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700 transition ease-in-out duration-150">
+                                    <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-yellow-400 bg-white dark:bg-gray-800 hover:bg-yellow-500 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700 transition ease-in-out duration-150">
                                         {{ Auth::user()->currentTeam->name }}
-
                                         <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
                                         </svg>
@@ -38,7 +37,7 @@
                             <x-slot name="content">
                                 <div class="w-60">
                                     <!-- Team Management -->
-                                    <div class="block px-4 py-2 text-xs text-gray-400">
+                                    <div class="block px-4 py-2 text-xs text-yellow-400">
                                         {{ __('Manage Team') }}
                                     </div>
 
@@ -57,7 +56,7 @@
                                     @if (Auth::user()->allTeams()->count() > 1)
                                         <div class="border-t border-gray-200 dark:border-gray-600"></div>
 
-                                        <div class="block px-4 py-2 text-xs text-gray-400">
+                                        <div class="block px-4 py-2 text-xs text-yellow-400">
                                             {{ __('Switch Teams') }}
                                         </div>
 
@@ -76,14 +75,13 @@
                     <x-dropdown align="right" width="48">
                         <x-slot name="trigger">
                             @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
-                                <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
+                                <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-yellow-500 transition">
                                     <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
                                 </button>
                             @else
                                 <span class="inline-flex rounded-md">
-                                    <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700 transition ease-in-out duration-150">
+                                    <button type="button" class="inline-flex items-center px-4 py-2 border border-transparent text-sm leading-4 font-medium rounded-lg text-gray-800 bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500 transition ease-in-out duration-150">
                                         {{ Auth::user()->name }}
-
                                         <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
                                         </svg>
@@ -94,12 +92,12 @@
 
                         <x-slot name="content">
                             <!-- Account Management -->
-                            <div class="block px-4 py-2 text-xs text-gray-400">
-                                {{ __('Manage Account') }}
+                            <div class="block px-4 py-2 text-xs text-yellow-400">
+                                {{ __('Administrar cuenta') }}
                             </div>
 
                             <x-dropdown-link href="{{ route('profile.show') }}">
-                                {{ __('Profile') }}
+                                {{ __('Perfil') }}
                             </x-dropdown-link>
 
                             @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
@@ -113,10 +111,8 @@
                             <!-- Authentication -->
                             <form method="POST" action="{{ route('logout') }}" x-data>
                                 @csrf
-
-                                <x-dropdown-link href="{{ route('logout') }}"
-                                         @click.prevent="$root.submit();">
-                                    {{ __('Log Out') }}
+                                <x-dropdown-link href="{{ route('logout') }}" @click.prevent="$root.submit();">
+                                    {{ __('Cerrar Sesión') }}
                                 </x-dropdown-link>
                             </form>
                         </x-slot>
@@ -126,7 +122,7 @@
 
             <!-- Hamburger -->
             <div class="-me-2 flex items-center sm:hidden">
-                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out">
+                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-yellow-400 hover:text-gray-500 dark:hover:text-yellow-500 hover:bg-gray-100 dark:hover:bg-yellow-500 focus:outline-none focus:bg-gray-100 dark:focus:bg-yellow-500 focus:text-gray-500 dark:focus:text-yellow-400 transition duration-150 ease-in-out">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                         <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                         <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
@@ -139,7 +135,7 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
+            <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')" class="text-yellow-400 hover:bg-yellow-500">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
         </div>
@@ -154,19 +150,19 @@
                 @endif
 
                 <div>
-                    <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
-                    <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+                    <div class="font-medium text-base text-yellow-400">{{ Auth::user()->name }}</div>
+                    <div class="font-medium text-sm text-yellow-400">{{ Auth::user()->email }}</div>
                 </div>
             </div>
 
             <div class="mt-3 space-y-1">
                 <!-- Account Management -->
-                <x-responsive-nav-link href="{{ route('profile.show') }}" :active="request()->routeIs('profile.show')">
+                <x-responsive-nav-link href="{{ route('profile.show') }}" :active="request()->routeIs('profile.show')" class="text-yellow-400 hover:bg-yellow-500">
                     {{ __('Profile') }}
                 </x-responsive-nav-link>
 
                 @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
-                    <x-responsive-nav-link href="{{ route('api-tokens.index') }}" :active="request()->routeIs('api-tokens.index')">
+                    <x-responsive-nav-link href="{{ route('api-tokens.index') }}" :active="request()->routeIs('api-tokens.index')" class="text-yellow-400 hover:bg-yellow-500">
                         {{ __('API Tokens') }}
                     </x-responsive-nav-link>
                 @endif
@@ -174,9 +170,7 @@
                 <!-- Authentication -->
                 <form method="POST" action="{{ route('logout') }}" x-data>
                     @csrf
-
-                    <x-responsive-nav-link href="{{ route('logout') }}"
-                                   @click.prevent="$root.submit();">
+                    <x-responsive-nav-link href="{{ route('logout') }}" @click.prevent="$root.submit();" class="text-yellow-400 hover:bg-yellow-500">
                         {{ __('Log Out') }}
                     </x-responsive-nav-link>
                 </form>
@@ -185,33 +179,13 @@
                 @if (Laravel\Jetstream\Jetstream::hasTeamFeatures())
                     <div class="border-t border-gray-200 dark:border-gray-600"></div>
 
-                    <div class="block px-4 py-2 text-xs text-gray-400">
-                        {{ __('Manage Team') }}
+                    <div class="block px-4 py-2 text-xs text-yellow-400">
+                        {{ __('Switch Teams') }}
                     </div>
 
-                    <!-- Team Settings -->
-                    <x-responsive-nav-link href="{{ route('teams.show', Auth::user()->currentTeam->id) }}" :active="request()->routeIs('teams.show')">
-                        {{ __('Team Settings') }}
-                    </x-responsive-nav-link>
-
-                    @can('create', Laravel\Jetstream\Jetstream::newTeamModel())
-                        <x-responsive-nav-link href="{{ route('teams.create') }}" :active="request()->routeIs('teams.create')">
-                            {{ __('Create New Team') }}
-                        </x-responsive-nav-link>
-                    @endcan
-
-                    <!-- Team Switcher -->
-                    @if (Auth::user()->allTeams()->count() > 1)
-                        <div class="border-t border-gray-200 dark:border-gray-600"></div>
-
-                        <div class="block px-4 py-2 text-xs text-gray-400">
-                            {{ __('Switch Teams') }}
-                        </div>
-
-                        @foreach (Auth::user()->allTeams() as $team)
-                            <x-switchable-team :team="$team" component="responsive-nav-link" />
-                        @endforeach
-                    @endif
+                    @foreach (Auth::user()->allTeams() as $team)
+                        <x-switchable-team :team="$team" component="x-responsive-nav-link" />
+                    @endforeach
                 @endif
             </div>
         </div>
