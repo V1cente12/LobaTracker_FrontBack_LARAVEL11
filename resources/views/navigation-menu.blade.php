95301e3e25da478226bf2679915a0817a7172ed0
V1cente12 <jvicentecv@gmail.com>
2024-09-12 12:53:39 -0400

    update nav

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -8,15 +8,7 @@
                     <a href="{{ route('dashboard') }}">
                         <img src="{{ asset('images/logo_loba.png') }}" alt="Logo Loba" class="block h-9 w-auto">
                     </a>
-                </div>
-
-                <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard') || request()->is('game/*')">
-                        {{ __('LOBA') }}
-                    </x-nav-link>
-                </div>
-                
+                </div>    
             </div>
 
             <div class="hidden sm:flex sm:items-center sm:ms-6">
@@ -135,12 +127,6 @@
 
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-        <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')" class="text-yellow-400 hover:bg-yellow-500">
-                {{ __('LOBA') }}
-            </x-responsive-nav-link>
-        </div>
-
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-yellow-200 dark:border-yellow-300 ">
             <div class="flex items-center px-4">
