d4a3d78830fffee7c0961688b18e71bfb16d48d5
V1cente12 <jvicentecv@gmail.com>
2024-09-13 00:09:08 -0400

    style dashboard

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,17 +1,18 @@
-<div class="max-w-sm sm:max-w-lg md:max-w-xl lg:max-w-2xl xl:max-w-screen-xl mx-auto bg-gradient-to-b from-gray-100 to-gray-300 p-4 shadow-lg rounded-lg mt-20">
-    <h1 class="text-2xl font-bold mb-4">Juegos Disponibles</h1>
-    <div class="flex flex-wrap -mx-2">
+<div class="max-w-sm sm:max-w-lg md:max-w-xl lg:max-w-2xl xl:max-w-screen-xl mx-auto rounded-lg mt-20">
+    <div class="flex flex-wrap justify-between gap-2 mb-2">
         @foreach($gameTypes as $gameType)
-            <div class="w-full md:w-1/3 px-2 mb-4">
-                <div wire:click="goToGameLobby({{ $gameType->id }})" class="cursor-pointer">
-                    <div class="bg-white rounded-lg shadow-md overflow-hidden">
-                        <img src="{{ asset($gameType->image_path) }}" alt="{{ $gameType->name }}" class="w-full h-48 object-cover">
-                        <div class="p-4 text-center">
-                            <h2 class="text-xl font-semibold">{{ $gameType->name }}</h2>
-                        </div>
+        <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-2 mb-4"> <!-- Ajustamos el ancho por pantalla -->
+            <div wire:click="goToGameLobby({{ $gameType->id }})" class="cursor-pointer">
+                <div class="rounded-lg overflow-hidden bg-white shadow-lg">
+                    <div class="w-full" style="aspect-ratio: 3 / 4;"> <!-- Usamos aspect-ratio para una relación de 3:4 -->
+                        <img src="{{ asset($gameType->image_path) }}" alt="{{ $gameType->name }}" class="w-full h-full object-cover"> <!-- Mantener object-cover para que las imágenes se ajusten bien -->
+                    </div>
+                    <div class="p-4 text-center"> <!-- Ajustamos el contenedor inferior para texto -->
+                        <h2 class="text-xl font-semibold">{{ $gameType->name }}</h2>
                     </div>
                 </div>
             </div>
+        </div>
         @endforeach
     </div>
 </div>
