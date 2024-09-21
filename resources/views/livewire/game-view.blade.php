c7f682cf7fc9d0c6668b7d4d7802511eb65018f8
V1cente12 <jvicentecv@gmail.com>
2024-09-20 21:38:24 -0400

    rejoin modal

--- resources/views/livewire/game-view.blade.php
+++ resources/views/livewire/game-view.blade.php
@@ -29,7 +29,7 @@
                                 @endif
                             </span>
                             <span class="text-lg mx-5">-</span>
-                            <span class="text-lg ml-1">{{ $player->total_points }}</span>
+                            <span class="text-lg ml-1">{{ $score->total }}</span>
                         </div>
                     @endforeach
                 </div>
@@ -37,42 +37,45 @@
         @endforeach
     </div>
 
-    <!-- Modal reportar puntos-->
+  <!-- Modal reportar puntos-->
     <div x-data="{ open: @entangle('showToReportPointsModal') }"
-         x-show="open"
-         @keydown.escape.window="open = false"
-         x-transition:enter="transition ease-out duration-300"
-         x-transition:enter-start="opacity-0 scale-90"
-         x-transition:enter-end="opacity-100 scale-100"
-         x-transition:leave="transition ease-in duration-200"
-         x-transition:leave-start="opacity-100 scale-100"
-         x-transition:leave-end="opacity-0 scale-95"
-         class="fixed inset-0 z-50 flex items-center justify-center bg-gray-900 bg-opacity-75">
+    x-show="open"
+    @keydown.escape.window="open = false"
+    x-transition:enter="transition ease-out duration-300"
+    x-transition:enter-start="opacity-0 scale-90"
+    x-transition:enter-end="opacity-100 scale-100"
+    x-transition:leave="transition ease-in duration-200"
+    x-transition:leave-start="opacity-100 scale-100"
+    x-transition:leave-end="opacity-0 scale-95"
+    class="fixed inset-0 z-50 flex items-center justify-center bg-gray-900 bg-opacity-75">
         <div class="bg-gradient-to-b from-[#199949] to-green-500 p-8 rounded-lg shadow-xl w-full max-w-lg">
-            <h2 class="text-xl font-semibold text-white mb-6">Reportar Puntos</h2>
-            <form wire:submit.prevent="reportPoints" class="space-y-6">
-                <input type="hidden" wire:model="selectedPlayerId">
-                <input type="hidden" wire:model="selectedGameId">
+        <h2 class="text-xl font-semibold text-white mb-6">Reportar Puntos</h2>
+        <form wire:submit.prevent="reportPoints" class="space-y-6">
+            <input type="hidden" wire:model="selectedPlayerId">
+            <input type="hidden" wire:model="selectedGameId">
 
-                <div>
-                    <label for="points" class="block text-base font-medium text-white">Puntaje</label>
-                    <input type="number" id="points" wire:model="points" class="mt-1 block w-full p-2 bg-gray-100 text-gray-900 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500" step="0.01" required>
-                    @error('points') <span class="text-red-500 text-sm">{{ $message }}</span> @enderror
-                </div>
-
-                <div class="flex justify-end space-x-4">
-                    <button type="button" wire:click="hideReportPointsModal" class="px-4 py-2 bg-gray-500 text-white rounded-lg hover:bg-gray-600 focus:ring focus:ring-gray-300">Cancelar</button>
-                    <button type="submit" class="px-4 py-2 bg-yellow-400 text-gray-800 rounded-lg hover:bg-yellow-500 focus:ring focus:ring-yellow-300">Reportar</button>
-                </div>
-            </form>
+            <div>
+                <label for="points" class="block text-base font-medium text-white">Puntaje</label>
+                <input type="number" id="points" wire:model="points" class="mt-1 block w-full p-2 bg-gray-100 text-gray-900 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500" step="0.01" required>
+                @error('points') <span class="text-red-500 text-sm">{{ $message }}</span> @enderror
+            </div>
+            <div class="flex justify-center space-x-4">
+                <button type="button" wire:click="hideReportPointsModal" class="px-4 py-2 bg-gray-500 text-white rounded-lg hover:bg-gray-600 focus:ring focus:ring-gray-300">Cancelar</button>
+                <button type="submit" class="px-4 py-2 bg-yellow-400 text-gray-800 rounded-lg hover:bg-yellow-500 focus:ring focus:ring-yellow-300">Reportar</button>
+            </div>
+        </form>
         </div>
     </div>
+
 
     <!-- Modal de carga -->
     <div>
         @if ($showToLoadingModal)
             <div wire:poll.4s="verifyAllPlayersReported" class="fixed inset-0 z-50 flex items-center justify-center bg-gray-900 bg-opacity-75">
                 <div class="bg-white p-8 rounded-lg shadow-xl w-full max-w-md text-center">
+                    <div class="flex justify-center mb-4">
+                        <div class="animate-spin rounded-full h-16 w-16 border-t-4 border-green-500 border-opacity-75"></div>
+                    </div>
                     <h2 class="text-xl font-semibold mb-6">Esperando a que todos los jugadores reporten sus puntos...</h2>
                     <p class="mt-4">Por favor, espera mientras verificamos si todos han reportado.</p>
                 </div>
@@ -80,33 +83,48 @@
         @endif
     </div>
 
-    <!-- Modal para ganador -->
+   <!-- Modal para ganador -->
     <div>
         @if ($showToWinnerModal)
             <div class="fixed inset-0 z-50 flex items-center justify-center bg-gray-900 bg-opacity-75">
-                <div class="bg-white p-8 rounded-lg shadow-xl w-full max-w-md text-center">
-                    <h2 class="text-xl font-semibold mb-4">Tenemos un ganador gente!</h2>
-                    <p class="text-lg mb-4">El ganador es:</p>
-                    <p class="text-2xl font-bold mb-4">{{ $winnerName }}</p>
-                    <p class="text-lg mb-6">Con un Prize Poll de: {{ $winnerTotal }} bs.</p>
-                    <button wire:click="closeWinnerModal" class="bg-blue-500 text-white px-4 py-2 rounded-lg">Cerrar</button>
+                <div class="bg-gradient-to-b from-[#199949] to-green-500 p-8 rounded-lg shadow-xl w-full max-w-md text-center">
+                    <h2 class="text-xl font-semibold text-white mb-4">¡Tenemos un ganador!</h2>
+                    <p class="text-lg text-white mb-4">El ganador es:</p>
+                    <p class="text-2xl font-bold text-white mb-4">{{ $winnerName }}</p>
+                    <p class="text-lg text-white mb-6">Con un Prize Pool de: {{ $winnerTotal }} bs.</p>
+                    <button wire:click="closeWinnerModal" class="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600">Cerrar</button>
                 </div>
             </div>
         @endif
     </div>
 
+    <!-- Modal de reenganche -->
     <div>
-    @if ($showToRejoinModal)
-        <div class="fixed inset-0 z-50 flex items-center justify-center bg-gray-900 bg-opacity-75">
-            <div class="bg-white p-8 rounded-lg shadow-xl w-full max-w-md text-center">
-                <h3 class="text-lg font-semibold">Necesitas Reenganche</h3>
-                <p>Te reengancharás con un puntaje de {{ $rejoinScore }}.</p>
-                <div class="flex justify-end">
-                    <button wire:click="acceptRejoin" class="bg-green-500 text-white px-4 py-2 rounded">Aceptar</button>
-                    <button wire:click="rejectRejoin" class="bg-red-500 text-white px-4 py-2 rounded ml-2">Rechazar</button>
+        @if ($showToRejoinModal)
+            <div class="fixed inset-0 z-50 flex items-center justify-center bg-gray-900 bg-opacity-75">
+                <div class="bg-gradient-to-b from-[#199949] to-green-500 p-8 rounded-lg shadow-xl w-full max-w-md text-center">
+                    <h3 class="text-xl font-semibold text-white mb-6">Necesitas Reenganche</h3>
+                    <p class="text-lg text-white mb-6">Te reengancharás con el puntaje de <strong>{{ $rejoinScore }}</strong>.</p>
+                    <!-- Centramos los botones -->
+                    <div class="flex justify-center space-x-4">
+                        <button wire:click="rejectRejoin" class="px-4 py-2 bg-gray-500 text-white rounded-lg hover:bg-gray-600">Rechazar</button>
+                        <button wire:click="acceptRejoin" class="px-4 py-2 bg-yellow-400 text-gray-800 rounded-lg hover:bg-yellow-500">Aceptar</button>
+                    </div>
                 </div>
             </div>
-        </div>
-    @endif
-</div>
+        @endif
+    </div>
+    <style>
+        .animate-spin-slow {
+            animation: spin 3s linear infinite;
+        }
+        @keyframes spin {
+            0% {
+                transform: rotate(0deg);
+            }
+            100% {
+                transform: rotate(360deg);
+            }
+        }
+    </style>
 </div>
