2279a96e3455b31601e049e5ee6765bb5f3efe7c
root <root@lobatrackerinstance.us-central1-c.c.task-manager-429923.internal>
2024-09-27 17:59:28 +0000

    change tittle

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ 'A la Note' }}</title>
+    <title>{{ 'Al Anote!' }}</title>
     <!-- Favicon -->
     <link rel="icon" href="{{ asset('images/logo_loba.ico') }}" type="image/x-icon">
     <!-- Fonts -->
