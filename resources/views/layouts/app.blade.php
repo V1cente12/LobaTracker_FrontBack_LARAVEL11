8aa47ea4c76feb315f152dae4cbb64e78d68794b
V1cente12 <jvicentecv@gmail.com>
2024-09-26 23:07:20 -0400

    fix title and ico

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ 'La Baraja Mágica' }}</title>
+    <title>{{ 'A la Note' }}</title>
     <!-- Favicon -->
     <link rel="icon" href="{{ asset('images/logo_loba.ico') }}" type="image/x-icon">
     <!-- Fonts -->
